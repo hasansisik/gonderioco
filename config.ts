e887c780a3cb32ff675bed33d996a7074c2f2f7c
Hasan Şişik <sisik.hsn@gmail.com>
2026-06-16 15:01:38 +0300

    v

--- config.ts
+++ config.ts
@@ -1,5 +1,3 @@
-export const server = process.env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_BACKEND_URL || "https://sadece-teklif-server.vercel.app/v1";
+export const server = process.env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3040/v1";
 export const socketServer = process.env.NEXT_PUBLIC_SERVER_URL || server.replace('/v1', '');
-
-
 
